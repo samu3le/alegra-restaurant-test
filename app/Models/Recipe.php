5e3636b0fd1b8d25f1fab189a9f42e2d8a7ac878
Samuel Espinoza <samuelesponline@gmail.com>
2022-02-14 22:37:25 -0500

    - [SECTION] recipe - [TYPE] add model

--- app/Models/Recipe.php
+++ app/Models/Recipe.php
@@ -8,4 +8,21 @@ use Illuminate\Database\Eloquent\Model;
 class Recipe extends Model
 {
     use HasFactory;
+
+
+    $table->bigIncrements('id');
+
+            $table->unsignedBigInteger('product_id');
+            $table->foreign('product_id')->references('id')->on('products');
+
+            $table->unsignedBigInteger('ingredient_id');
+            $table->foreign('ingredient_id')->references('id')->on('ingredients');
+
+            $table->integer('quantity');
+
+            $table->boolean('is_active')->default(1);
+
+            $table->unsignedBigInteger('created_by')->nullable();
+            $table->foreign('created_by')->references('id')->on('users');
+            
 }
