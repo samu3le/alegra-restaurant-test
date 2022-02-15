215dcd9f853dc074b361fc9f00a698d26ce222b1
Samuel Espinoza <samuelesponline@gmail.com>
2022-02-14 22:06:16 -0500

    - [SECTION]  middleware - [TYPE] Update validations Add validator custom iunique

--- routes/api.php
+++ routes/api.php
@@ -102,5 +102,30 @@ Route::prefix('v1')->middleware([DataParser::class])->group(function () {
 
         });
 
+        Route::prefix('recipes')->group(function () {
+
+            Route::middleware([
+                Validations\Requests\Pagination::class,
+                Requests\ProductValidation\GetAll::class
+            ])
+            ->get('get_all',  Controllers\ProductController\GetAll::class);
+
+            Route::middleware([
+                Requests\ProductValidation\Find::class
+            ])
+            ->get('find',  Controllers\ProductController\Find::class);
+
+            Route::middleware([
+                Requests\ProductValidation\Create::class
+            ])
+            ->post('create', Controllers\ProductController\Create::class);
+
+            Route::middleware([
+                Requests\ProductValidation\Update::class
+            ])
+            ->post('update', Controllers\ProductController\Update::class);
+
+        });
+
     // });
 });
