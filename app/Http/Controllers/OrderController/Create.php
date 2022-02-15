94833c07702b6520fc7611b69e9677e0b0af930a
Samuel Espinoza <samuelesponline@gmail.com>
2022-02-15 14:43:31 -0500

    - [SECTION] Order Details - [TYPE] Update Update create and update with orders details

--- app/Http/Controllers/OrderController/Create.php
+++ app/Http/Controllers/OrderController/Create.php
@@ -18,40 +18,48 @@ class Create extends Controller
         $body = $request['body'];
         $order = new Order;
 
-        $order->quantity = $body['quantity'];
-
-        if(isset($body['state'])){
-            $order->state = $body['state'];
-        }
-
-        $order->save();
-
         $random = Product::select('id')
-                ->where('is_active','true')
-                ->inRandomOrder()
-                ->limit($order->quantity) // here is yours limit
-                ->pluck('id')->toArray();
+            ->where('is_active','true')
+            ->inRandomOrder()
+            ->limit($order->quantity) // here is yours limit
+            ->pluck('id')->toArray();
 
-        $data_insert=[];
-        for($i=0; $i < $order->quantity; $i++){
-            for ($j=0; $j<sizeof($random); $j++) {
-                if(sizeof($data_insert)==$order->quantity){
-                    break;
-                }
-                $data_insert[] = [
-                    'order_id' => $order->id,
-                    'product_id' => $random[$j]
-                ];
+        if(sizeof($random) != 0){
+            $order->quantity = $body['quantity'];
+
+            if(isset($body['state'])){
+                $order->state = $body['state'];
             }
+
+            $order->save();
+
+            $data_insert=[];
+            for($i=0; $i < $order->quantity; $i++){
+                for ($j=0; $j<sizeof($random); $j++) {
+                    if(sizeof($data_insert)==$order->quantity){
+                        break;
+                    }
+                    $data_insert[] = [
+                        'order_id' => $order->id,
+                        'product_id' => $random[$j]
+                    ];
+                }
+            }
+
+            OrderDetails::insert($data_insert);
+
+            return Response::CREATED(
+                message: 'Order created successfully.',
+                data: [
+                    'order' => $order,
+                ]
+            );
+        }else{
+            return Response::UNPROCESSABLE_ENTITY(
+                message: 'Validation failed.',
+                errors: 'Products is empty',
+            );
         }
 
-        OrderDetails::insert($data_insert);
-
-        return Response::CREATED(
-            message: 'Order created successfully.',
-            data: [
-                'order' => $order,
-            ]
-        );
     }
 }
--- app/Http/Controllers/OrderController/Update.php
+++ app/Http/Controllers/OrderController/Update.php
@@ -7,6 +7,8 @@ use Illuminate\Http\Request;
 
 use App\Services\Response;
 use App\Models\Order;
+use App\Models\OrderDetails;
+use App\Models\Product;
 
 class Update extends Controller
 {
@@ -16,20 +18,58 @@ class Update extends Controller
 
         $order = Order::find($body['id']);
 
-        if(isset($body['quantity'])){
-            $order->quantity = $body['quantity'];
-        }
-        if(isset($body['state'])){
-            $order->state = $body['state'];
-        }
+        $random = Product::select('id')
+            ->where('is_active','true')
+            ->inRandomOrder()
+            ->limit($order->quantity) // here is yours limit
+            ->pluck('id')->toArray();
 
-        $order->save();
+        if(sizeof($random) != 0){
 
-        return Response::OK(
-            message: 'Order updated successfully.',
-            data: [
-                'order' => $order,
-            ]
-        );
+            if((isset($body['state'])) && ($body['state'] != $order->state)){
+                $order->state = $body['state'];
+            }
+
+            if(isset($body['quantity']) && ($order->state ==1)){
+                $order->quantity = $body['quantity'];
+
+                $get_order_details = OrderDetails::where('order_id', $order->id)
+                ->get();
+
+                foreach($get_order_details as $order_details_dlt){
+                    $order_details_dlt->delete();
+                }
+
+                $data_insert=[];
+                for($i=0; $i < $order->quantity; $i++){
+                    for ($j=0; $j<sizeof($random); $j++) {
+                        if(sizeof($data_insert)==$order->quantity){
+                            break;
+                        }
+                        $data_insert[] = [
+                            'order_id' => $order->id,
+                            'product_id' => $random[$j]
+                        ];
+                    }
+                }
+
+                OrderDetails::insert($data_insert);
+            }
+
+            $order->save();
+
+            return Response::OK(
+                message: 'Order updated successfully.',
+                data: [
+                    'order' => $order,
+                ]
+            );
+
+        }else{
+            return Response::UNPROCESSABLE_ENTITY(
+                message: 'Validation failed.',
+                errors: 'Products is empty',
+            );
+        }
     }
 }
