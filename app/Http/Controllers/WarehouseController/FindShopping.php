64d570ad9888c1276018c70c50ce61e8ea5fefbe
Samuel Espinoza <samuelesponline@gmail.com>
2022-02-19 14:34:42 -0500

    Fix query - [SECTION] FindShopping - [TYPE] fix

--- app/Http/Controllers/WarehouseController/FindShopping.php
+++ app/Http/Controllers/WarehouseController/FindShopping.php
@@ -7,22 +7,43 @@ use Illuminate\Http\Request;
 
 use App\Services\Response;
 
+use App\Models\ShoppingHistory;
 use App\Models\Ingredient;
 
 class FindShopping extends Controller
 {
     public function __invoke(Request $request)
     {
-        $query = $request['query'];
 
-        $shoppings = Ingredient::find($query['id']);
-        $shoppings = $shoppings->shoppings->each(function($shopping){
-            $shopping->owner;
-        });
+        $query = $request['query'];
+        $page = $query['page'];
+        $per_page = $query['per_page'];
+
+
+        $shoppingHistory = ShoppingHistory::where('ingredient_id',$query['id']);
+        $shoppingHistory = $shoppingHistory->with('owner');
+
+        $ingredient =  Ingredient::find($query['id']);
+
+        if(isset($query['sort_by'])){
+            $shoppingHistory = $shoppingHistory->orderBy($query['sort_by'], $shoppingHistory['sort']);
+        }
+
+        $shoppingHistory = $shoppingHistory->paginate(
+            $per_page, // per page (may be get it from request)
+            ['*'], // columns to select from table (default *, means all fields)
+            'page', // page name that holds the page number in the query string
+            $page // current page, default 1
+        );
+
+        // $shoppingHistory = $shoppingHistory->shoppings->each(function($shopping){
+        //     $shopping->owner;
+        // });
+
         return Response::OK(
             message: 'Shopping history found successfully.',
             data: [
-                'shoppings' => $shoppings,
+                'shoppings' => $shoppingHistory,
             ]
         );
     }
